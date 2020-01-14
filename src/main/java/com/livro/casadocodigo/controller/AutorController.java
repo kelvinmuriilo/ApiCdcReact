3fe0a93ea503ca16f87f1927cb37763baf3b9c37
unknown <kelvinmuriloas@gmail.com>
2020-01-14 00:15:47 -0300

    configurando cross

--- src/main/java/com/livro/casadocodigo/controller/AutorController.java
+++ src/main/java/com/livro/casadocodigo/controller/AutorController.java
@@ -6,6 +6,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.livro.casadocodigo.cmd.CadastroAutorCmd;
@@ -21,6 +23,7 @@ import com.livro.casadocodigo.servico.CasaDoCodigoServico;
 
 @RestController
 @RequestMapping("/autores")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AutorController {
 	
 	@Autowired
@@ -31,7 +34,7 @@ public class AutorController {
 		return cdcServico.getListaAutores();
 	}
 	
-	@PostMapping
+	@PostMapping()
 	@Transactional
 	public ResponseEntity<String> cadastrarAutor(@RequestBody CadastroAutorCmd cmd){
 		cdcServico.cadastrarAutor(cmd);
